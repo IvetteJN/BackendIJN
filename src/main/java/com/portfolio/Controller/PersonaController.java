5c2d2e183d4ebe14abb0026143d749700a5772c2
Ivette <nobiltaivette@gmail.com>
2022-10-20 23:36:55 -0300

    cors corrections

--- src/main/java/com/portfolio/Controller/PersonaController.java
+++ src/main/java/com/portfolio/Controller/PersonaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/persona")
-@CrossOrigin(origins = {"https://.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontendijn.web.app","http://localhost:4200"})
 public class PersonaController {
     @Autowired
     ImpPersonaService sPersona;
